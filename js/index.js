1b0a318838d9018915902517c55752aa8265bf0e
Sydney Flak <sflak@porter-kresge-apts-57-159.resnet.ucsc.edu>
2018-02-27 16:46:50 -0800

    Upload csv or paste share link

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 
 let fileInfoArea = $('#filename');
+let deleteFile = $('#deleteFile');
 let fileInput = $('#file').change(function(event){
     let name = event.target.value.split('\\').pop();
+    deleteFile.css('display', 'inline-block');
+    fileInfoArea.css('display', 'inline-block');
     fileInfoArea.text(name);
+    $('#driveLink').prop('disabled', true);
 });
 
+deleteFile.click(function(event) {
+    deleteFile.css('display', 'none');
+    fileInfoArea.css('display', 'none');
+    $('#file').val('');
+    $('#driveLink').prop('disabled', false);
+});
+
+// if($('#driveLink').val()){
+//     $('#fileInput').prop('disabled', true);
+// }
+
+let driveLink = $('#driveLink').change(function(event){
+    console.log('change');
+    console.log(event.target.value);
+    let input = event.target.value;
+    if(input.length > 0){
+        console.log('set disabled')
+        $('#fileInput').prop('disabled', true);
+    }
+    else {
+        console.log('set enabled')
+        $('#fileInput').prop('disabled', false);
+    }
+    
+});
