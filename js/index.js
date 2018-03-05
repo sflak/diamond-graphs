b087d20a1da3ed3327dbaf7d05098e3f68472bb0
Sydney Flak <sflak@porter-kresge-apts-59-67.resnet.ucsc.edu>
2018-03-05 10:47:18 -0800

    add instructions

--- js/index.js
+++ js/index.js
@@ -8,12 +8,15 @@ let fileInput = $('#file').change(function(event){
     fileInfoArea.css('display', 'inline-block');
     fileInfoArea.text(name);
     $('#driveLink').prop('disabled', true);
+    $('#driveLink').removeClass('invalid');
+    $('#errMsg').remove();
 });
 
 
 // IIFE sets the correct instructions even on re-load
 (function(){
     setDataFormatInstructions()
+    
 }());
 // Change instructions on select
 $('#graph-type').change(function(){
@@ -26,11 +29,11 @@ function setDataFormatInstructions (){
     let options = $('<div class="inputs__options"><p>Graph options...</p></div>');
     switch(currentGraphType){
         case('bar'):
-            instr = $('<p class="inputs__main__instruc">[BAR INSTRUCTIONS HERE]</p>');
+            instr = $('<p class="inputs__main__instruc">Data should be in two columns. The first column will be the x values, the second the y values. The first row should be the  labels for each axis.</p>');
             options.append($('<input id="title" type="text" name="title" placeholder="Title" value="">'))
             break;
         case('line'):
-            instr = $('<p class="inputs__main__instruc">[LINE INSTRUCTIONS HERE]</p>');
+            instr = $('<p class="inputs__main__instruc">Data should be in two columns. The first column will be the x values, the second the y values. The first row should be the  labels for each axis.</p>');
             options.append($('<p>none</p>'));
             break;
         case('map'):
@@ -42,11 +45,11 @@ function setDataFormatInstructions (){
             options.append($('<input id="title" type="text" name="title" placeholder="Title" value="">'));
             break;
         case('timeseries'):
-            instr = $('<p class="inputs__main__instruc">Data should be a single column of dates in the format YYYY/MM/DD</p>');
+            instr = $('<p class="inputs__main__instruc">Data should be a single column of dates in the format YYYY-MM-DD-HH-MM</p>');
             options.append($('<p>none</p>'));
             break;
         case('piechart'):
-            instr = $('<p class="inputs__main__instruc">[PIE INSTRUCTIONS HERE]</p>');
+            instr = $('<p class="inputs__main__instruc">Data should be in two columns. The left is a column of labels, the right are the associated values.</p>');
             options.append($('<input id="title" type="text" name="title" placeholder="Title" value="My Pie Chart">'));
             options.append($('<input id="caption" type="text" name="title" placeholder="Caption" value="">'));
             break;
@@ -65,12 +68,20 @@ deleteFile.click(function(event) {
     fileInfoArea.css('display', 'none');
     $('#file').val('');
     $('#driveLink').prop('disabled', false);
+
+    validateLinkInput($('#driveLink').val());
+
 });
 
 
 let driveLink = $('#driveLink').change(function(event){
     let input = event.target.value;
+    validateLinkInput(input);
     
+});
+
+const validateLinkInput = (input) => {
+
     let valid = null;
     if(input.length > 0){
         $('#fileInput').prop('disabled', true);
@@ -79,7 +90,7 @@ let driveLink = $('#driveLink').change(function(event){
         valid = regEx.test(input);
 
         if(!valid){
-            $('<p class="inputs__errorText">Must be a valid sheets link</p>').insertAfter($('#driveLink'));
+            $('<p class="inputs__errorText" id="errMsg">Must be a valid sheets link</p>').insertAfter($('#driveLink'));
             $('#driveLink').addClass('invalid');
         }
         else{
@@ -91,8 +102,7 @@ let driveLink = $('#driveLink').change(function(event){
     else {
         $('#fileInput').prop('disabled', false);
     }
-    
-});
+}
 
 
 var script3 = document.createElement("script"),
